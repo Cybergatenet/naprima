ec7a7b40ef4a788d289fde3cad96fd4aeddf06d1
Cybergatenet <abelchinedu2@gmail.com>
2025-10-01 18:14:09 -0400

    configured image upload

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -43,7 +43,7 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: "Blog was successfully created." }
+        format.html { redirect_to @blog, notice: "Blog was successfully created. Image uplaoded" }
         format.json { render :show, status: :created, location: @blog }
       else
         format.html { render :new, status: :unprocessable_entity }
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,7 @@
 class Blog < ApplicationRecord
   has_many :comments, dependent: :destroy
+
+  has_one_attached :image  # for a single image upload
+
+  # has_many_attached :images # for multiple files
 end
